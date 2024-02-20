9b5d07d674e3ab9d6400c57ae1b61f9052fb310b
Miguel Pérez <fpertru695@g.educaand.es>
2024-02-20 21:27:28 +0100

    Corrección errores

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/personajes');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
